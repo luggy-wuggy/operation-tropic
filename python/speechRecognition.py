98260b9351d4e20c829ed9e65d53a6fa7cd163c1
Luqman Abdurrohman <luqmanabdurrohman@Luqmans-MacBook-Air.local>
2019-10-05 19:34:54 -0700

    pushing files

---
+++ python/speechRecognition.py
@@ -0,0 +1,101 @@
+
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import playsound
+import speech_recognition as sr 
+from gtts import gTTS
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+##takes in a string paramenter and will
+##save it as a .mp3 file and play it
+def speak(text):
+	tts = gTTS(text=text, lang="en")   # transform the text into an audio file with english lanuage
+	filename = "voice.mp3"
+	tts.save(filename)
+	playsound.playsound(filename)
+
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		said = ""
+
+		try:
+			said = r.recognize_google(audio)
+			print(said)
+		except Exception as e:
+			print("Exception: " + str(e))
+
+	return said
+
+## Authenticates the user and returns a service that contains the google calendar information
+def authenticate_google():
+
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+
+## n : number of events to get
+## service : service that was returned when user is authenticated
+def get_events(n ,service):
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print(f'Getting the upcoming {n} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=n, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+
+service = authenticate_google()
+get_events(2, service)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
